affa31c74895e51b1ea556a87b15af6a87ed1181
SIMPLE-TECNIQUES <shankarcode2000@gmail.com>
2024-11-11 11:57:32 +0530

    path changes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux';
 import App from './App'
-import store from './Redux/Store';
+import store from './Redux/store';
 
 
 const rootElement = document.getElementById('root');
